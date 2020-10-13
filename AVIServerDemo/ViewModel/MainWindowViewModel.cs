a349c531df749434cae28093491f7437c1c03244
鑫建 孙 <xinjian_sun@sina.com>
2020-10-13 14:38:54 +0800

    添加项目文件。

---
+++ AVIServerDemo/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVIServerDemo.ViewModel
+{
+    class MainWindowViewModel : NotificationObject
+    {
+        #region 属性绑定
+        private string version;
+
+        public string Version
+        {
+            get { return version; }
+            set
+            {
+                version = value;
+                this.RaisePropertyChanged("Version");
+            }
+        }
+        #endregion
+        #region 构造函数
+        public MainWindowViewModel()
+        {
+            #region 初始化参数
+            Version = "20201013";
+            #endregion
+        }
+        #endregion
+
+
+    }
+}
