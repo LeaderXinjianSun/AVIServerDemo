108f184fd08afca8910b1c14d35c29d51d630678
鑫建 孙 <xinjian_sun@sina.com>
2020-11-03 13:47:41 +0800

    服务端调试备份

--- AVIServerDemo/ViewModel/MainWindowViewModel.cs
+++ AVIServerDemo/ViewModel/MainWindowViewModel.cs
@@ -8,6 +8,8 @@ using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using ViewROI;
@@ -151,7 +153,28 @@ namespace AVIServerDemo.ViewModel
                 this.RaisePropertyChanged("CameraGCStyle");
             }
         }
+        private string serverIP;
 
+        public string ServerIP
+        {
+            get { return serverIP; }
+            set
+            {
+                serverIP = value;
+                this.RaisePropertyChanged("ServerIP");
+            }
+        }
+        private int serverPort;
+
+        public int ServerPort
+        {
+            get { return serverPort; }
+            set
+            {
+                serverPort = value;
+                this.RaisePropertyChanged("ServerPort");
+            }
+        }
 
         #endregion
         #region 方法绑定
@@ -162,29 +185,37 @@ namespace AVIServerDemo.ViewModel
         #endregion
         #region 变量
         private string iniParameterPath = System.Environment.CurrentDirectory + "\\Parameter.ini";
+        DXH.Net.DXHTCPServer tcpServer;
         #endregion
         #region 构造函数
         public MainWindowViewModel()
         {
             #region 初始化参数
-            Version = "20201023";
+            Version = "20201103";
             MessageStr = "";
             HomePageVisibility = "Visible";
             ParameterPageVisibility = "Collapsed";
             WorkPath = Inifile.INIGetStringValue(iniParameterPath, "System", "WorkPath", "D:\\");
             ImageSavePath = Inifile.INIGetStringValue(iniParameterPath, "System", "ImageSavePath", "D:\\");
             CameraROIList = new ObservableCollection<ROI>();
+
+            ServerIP = Inifile.INIGetStringValue(iniParameterPath, "Server", "IP", "192.168.0.11");
+            ServerPort = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "Server", "PORT", "3000"));
             #endregion
             AppLoadedEventCommand = new DelegateCommand(new Action(this.AppLoadedEventCommandExecute));
             MenuActionCommand = new DelegateCommand<object>(new Action<object>(this.MenuActionCommandExecute));
             FolderBrowserDialogCommand = new DelegateCommand<object>(new Action<object>(this.FolderBrowserDialogCommandExecute));
             ParameterSaveCommand = new DelegateCommand(new Action(this.ParameterSaveCommandExecute));
+
+
         }
 
         private void ParameterSaveCommandExecute()
         {
             Inifile.INIWriteValue(iniParameterPath, "System", "WorkPath", WorkPath);
             Inifile.INIWriteValue(iniParameterPath, "System", "ImageSavePath", ImageSavePath);
+            Inifile.INIWriteValue(iniParameterPath, "Server", "IP", ServerIP);
+            Inifile.INIWriteValue(iniParameterPath, "Server", "PORT", ServerPort.ToString());
             AddMessage("保存参数");
         }
 
@@ -253,64 +284,65 @@ namespace AVIServerDemo.ViewModel
                 AddMessage($"数据库连接失败{ex.Message}");
                 StatusDataBase = false;
             }
+            tcpServer = new DXH.Net.DXHTCPServer();
+            tcpServer.LocalIPAddress = Inifile.INIGetStringValue(iniParameterPath, "Server", "IP", "127.0.0.1");
+            tcpServer.LocalIPPort = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "Server", "PORT", "11001"));
+            tcpServer.SocketListChanged += TcpServer_SocketListChanged;
+            tcpServer.ConnectStateChanged += TcpServer_ConnectStateChanged;
+            tcpServer.Received += TcpServer_Received;
+            tcpServer.StartTCPListen();
             Run();
         }
-        #endregion
-        #region 自定义函数
-        private void AddMessage(string str)
+
+        private void TcpServer_Received(object sender, string e)
         {
-            string[] s = MessageStr.Split('\n');
-            if (s.Length > 1000)
-            {
-                MessageStr = "";
-            }
-            if (MessageStr != "")
-            {
-                MessageStr += "\n";
-            }
-            MessageStr += System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " " + str;
-        }
-        private async void Run()
-        {
-            while (true)
-            {
+            string restr = e.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)[0];
+            AddMessage("接收:" + restr);
+            Task.Run(()=> {
                 try
                 {
                     if (Directory.Exists(WorkPath))
                     {
                         DirectoryInfo folder = new DirectoryInfo(WorkPath);
-                        FileInfo[] files = folder.GetFiles("*.bmp");
-                        if (files.Length > 0 && !IsFileLocked(files[0]))
+                        int cout = 0;
+                        FileInfo[] files = folder.GetFiles(restr);
+                        while (!(files.Length > 0 && !IsFileLocked(files[0])) && cout < 10)
                         {
-                            string[] vs = files[0].Name.Split(new string[] { "_"}, StringSplitOptions.RemoveEmptyEntries);
-                            if (vs.Length == 2)
+                            files = folder.GetFiles(restr);
+                            cout++;
+                            System.Threading.Thread.Sleep(200);
+                        }
+                        if (cout < 10)
+                        {
+                            string[] vs = files[0].Name.Split(new string[] { "_" }, StringSplitOptions.RemoveEmptyEntries);
+                            if (vs.Length == 3)
                             {
-
                                 HObject img;
                                 HOperatorSet.ReadImage(out img, files[0].FullName);
                                 CameraIamge = new HImage(img);
 
-                                string[] vs1 = vs[1].Split(new string[] { "." }, StringSplitOptions.RemoveEmptyEntries);
-                                if (!Directory.Exists(Path.Combine(ImageSavePath, vs1[0])))
+                                
+                                if (!Directory.Exists(Path.Combine(ImageSavePath, vs[1])))
                                 {
-                                    Directory.CreateDirectory(Path.Combine(ImageSavePath, vs1[0]));
+                                    Directory.CreateDirectory(Path.Combine(ImageSavePath, vs[1]));
                                 }
-
-                                File.Copy(files[0].FullName, Path.Combine(ImageSavePath, vs1[0], files[0].Name), true);
+                                string[] vs1 = vs[2].Split(new string[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+                                if (!Directory.Exists(Path.Combine(ImageSavePath, vs[1], vs1[0])))
+                                {
+                                    Directory.CreateDirectory(Path.Combine(ImageSavePath, vs[1], vs1[0]));
+                                }
+                                File.Copy(files[0].FullName, Path.Combine(ImageSavePath, vs[1], vs1[0], files[0].Name), true);
                                 string str = files[0].FullName;
                                 File.Delete(str);
 
                                 Mysql mysql = new Mysql();
-                                if (await Task.Run<bool>(() => { return mysql.Connect(); }))
-                                {                                    
-                                    for (int i = 0; i < 16; i++)
+                                if (mysql.Connect())
+                                {
+                                    string stm = $"INSERT INTO aviproductdata (BoardID,PcsIndex,ResultItem1) VALUES ('{vs[0]}',{vs1[0]},'OK')";
+                                    int result = mysql.executeQuery(stm);
+                                    if (result < 1)
                                     {
-                                        string stm = $"INSERT INTO aviproductdata (BoardID,PcsIndex,ResultItem1) VALUES ('{vs1[0]}',{i},'OK')";
-                                        int result = mysql.executeQuery(stm);
-                                        if (result < 1)
-                                        {
-                                            AddMessage("请注意，数据库插入失败");
-                                        }
+                                        AddMessage("请注意，数据库插入失败");
                                     }
                                     StatusDataBase = true;
                                 }
@@ -333,15 +365,47 @@ namespace AVIServerDemo.ViewModel
                         }
                         else
                         {
-                            await Task.Delay(500);
+                            AddMessage($"获取文件{restr}超时");
                         }
                     }
-                    
+
                 }
                 catch (Exception ex)
                 {
                     AddMessage(ex.Message);
                 }
+            });
+        }
+
+        private void TcpServer_ConnectStateChanged(object sender, string e)
+        {
+            AddMessage("TCP Server " + e);
+        }
+
+        private void TcpServer_SocketListChanged(object sender, bool e)
+        {
+            AddMessage($"客户端:{((IPEndPoint)(((Socket)sender).RemoteEndPoint)).Address}:{((IPEndPoint)(((Socket)sender).RemoteEndPoint)).Port} {(e ? "连接" : "断开")}");
+        }
+        #endregion
+        #region 自定义函数
+        private void AddMessage(string str)
+        {
+            string[] s = MessageStr.Split('\n');
+            if (s.Length > 1000)
+            {
+                MessageStr = "";
+            }
+            if (MessageStr != "")
+            {
+                MessageStr += "\n";
+            }
+            MessageStr += System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " " + str;
+        }
+        private async void Run()
+        {
+            while (true)
+            {
+                
                 await Task.Delay(100);
             }
         }
